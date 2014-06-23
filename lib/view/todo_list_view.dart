8f37aa7ba6e56f1a6e0a8c9186e1423dd4e64f4d
dotdotcommadot <hans@dotdotcommadot.com>
2014-06-23 17:44:31 +0200

    adds description field

--- lib/view/todo_list_view.dart
+++ lib/view/todo_list_view.dart
@@ -4,6 +4,7 @@ import 'package:robotlegs_polymer/model/vo/todo_item.dart';
 import 'package:observe/observe.dart';
 import 'package:polymer/polymer.dart';
 import 'dart:async';
+import 'dart:html' as dom;
 
 @CustomTag('todo-list-view')
 class TodoListView extends PolymerElement
@@ -43,6 +44,7 @@ class TodoListView extends PolymerElement
   
   void addTodo()
   {
-  	addTodoButtonClickedController.add('hello');
+  	dom.InputElement descriptionInput = shadowRoot.querySelector('#description-input');
+  	addTodoButtonClickedController.add(descriptionInput.value);
   }
 }
